6f6fba12dbda603648bedfa7e7b4bba8908bcc92
lyearling <ly2033a@american.edu>
2024-03-01 13:19:17 -0500

    added function count_na

---
+++ forloop/R/practice.R
@@ -0,0 +1,34 @@
+col_means <- function(df) {
+  num_cols <- ncol(df)
+  means <- numeric(num_cols)
+  
+  for (i in 1:num_cols) {
+    means[i] <- mean(df[[i]]) 
+  }
+  
+  return(means)
+}
+
+col_means <- function(df) {
+  num_cols <- ncol(df)
+  means <- numeric(num_cols)
+  
+  for (i in 1:num_cols) {
+    means[i] <- mean(df[[i]]) 
+  }
+  
+  return(means)
+}
+
+
+count_na <- function(vec) {
+  count <- 0  
+  
+  for (element in vec) {
+    if (is.na(element)) {
+      count <- count + 1
+    }
+  }
+  
+  return(count)
+}
